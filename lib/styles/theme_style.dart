c75334c418c42f4d73b5e788f9c6870eca6b8d03
AndersonSantanaMjv <anderson.santana@mjv.com.br>
2023-06-28 20:57:47 -0300

    controle de themas

--- lib/styles/theme_style.dart
+++ lib/styles/theme_style.dart
@@ -27,13 +27,52 @@ class MyTheme {
     ),
     switchTheme: SwitchThemeData(
       thumbColor: MaterialStateColor.resolveWith((states) {
-        return states.contains(MaterialState.selected) ? _light.primary : _light.secondary;
+        return states.contains(MaterialState.selected)
+            ? _light.primary
+            : _light.secondary;
       }),
       trackColor: MaterialStateColor.resolveWith((states) {
-        return states.contains(MaterialState.selected) ? _light.primaryDark : _light.secondary;
+        return states.contains(MaterialState.selected)
+            ? _light.primaryDark
+            : _light.secondary;
       }),
     ),
   );
 
-  static final ThemeData escuro = ThemeData.dark().copyWith();
+  static final ThemeData escuro = ThemeData.dark().copyWith(
+    colorScheme: ThemeData.dark().colorScheme.copyWith(
+          primary: _dark.primary,
+          secondary: _dark.accent,
+          background: _dark.backgroud,
+          error: _dark.error,
+        ),
+    primaryColor: _dark.primary,
+    appBarTheme: AppBarTheme(
+      backgroundColor: _dark.primaryDark,
+      titleTextStyle: TextStyle(
+        color: _dark.text,
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    ),
+    cardColor: _dark.backgroudDark,
+    checkboxTheme: CheckboxThemeData(
+      fillColor: MaterialStateColor.resolveWith((states) => _dark.primary),
+    ),
+    dialogTheme: DialogTheme(
+      titleTextStyle: TextStyle(color: _dark.text),
+    ),
+    switchTheme: SwitchThemeData(
+      thumbColor: MaterialStateColor.resolveWith((states) {
+        return states.contains(MaterialState.selected)
+            ? _dark.primary
+            : _dark.secondary;
+      }),
+      trackColor: MaterialStateColor.resolveWith((states) {
+        return states.contains(MaterialState.selected)
+            ? _dark.primaryDark
+            : _dark.secondary;
+      }),
+    ),
+  );
 }
